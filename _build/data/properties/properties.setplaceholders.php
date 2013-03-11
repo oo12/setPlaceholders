5639549821983077904f0d975d9e30ef3228281c
Jason Grant <dadima@gmail.com>
2013-03-10 20:30:11 -0500

    version 1.2

--- _build/data/properties/properties.setplaceholders.php
+++ _build/data/properties/properties.setplaceholders.php
@@ -3,70 +3,96 @@
 /**
  * Default properties for the setPlaceholders snippet
  * @author Jason Grant
- * 12/30/12
  *
  * @package setplaceholders
  * @subpackage build
  */
 
 $properties = array(
-    array(
-          'name' => 'delimiter',
-          'desc' => 'prop_sph.delimiter_desc',
-          'type' => 'textfield',
-          'options' => '',
-          'value' => ',',
-          'lexicon' => 'setplaceholders:default'
-          ),
-    array(
-          'name' => 'fields',
-          'desc' => 'prop_sph.fields_desc',
-          'type' => 'textfield',
-          'options' => '',
-          'value' => '',
-          'area' => 'Deprecated',
-          'lexicon' => 'setplaceholders:default'
-          ),
-    array(
-          'name' => 'id',
-          'desc' => 'prop_sph.id_desc',
-          'type' => 'integer',
-          'options' => '',
-          'value' => '',
-          'lexicon' => 'setplaceholders:default'
-          ),
-    array(
-          'name' => 'output',
-          'desc' => 'prop_sph.output_desc',
-          'type' => 'combo-boolean',
-          'options' => '',
-          'value' => '0',
-          'lexicon' => 'setplaceholders:default'
-          ),
-    array(
-          'name' => 'placeholders',
-          'desc' => 'prop_sph.placeholders_desc',
-          'type' => 'textfield',
-          'options' => '',
-          'value' => '',
-          'lexicon' => 'setplaceholders:default'
-          ),
-    array(
-          'name' => 'prefix',
-          'desc' => 'prop_sph.prefix_desc',
-          'type' => 'textfield',
-          'options' => '',
-          'value' => 'sph.',
-          'lexicon' => 'setplaceholders:default'
-          ),
-    array(
-          'name' => 'ph',
-          'desc' => 'prop_sph.ph_desc',
-          'type' => 'textfield',
-          'options' => '',
-          'value' => '',
-          'lexicon' => 'setplaceholders:default'
-          )
+	array(
+		'name' => 'delimiter',
+		'desc' => 'prop_sph.delimiter_desc',
+		'type' => 'textfield',
+		'options' => '',
+		'value' => ',',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'fields',
+		'desc' => 'prop_sph.fields_desc',
+		'type' => 'textfield',
+		'options' => '',
+		'value' => '',
+		'area' => 'Deprecated',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'id',
+		'desc' => 'prop_sph.id_desc',
+		'type' => 'integer',
+		'options' => '',
+		'value' => '',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'output',
+		'desc' => 'prop_sph.output_desc',
+		'type' => 'combo-boolean',
+		'options' => '',
+		'value' => '0',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'placeholders',
+		'desc' => 'prop_sph.placeholders_desc',
+		'type' => 'textfield',
+		'options' => '',
+		'value' => '',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'prefix',
+		'desc' => 'prop_sph.prefix_desc',
+		'type' => 'textfield',
+		'options' => '',
+		'value' => 'sph.',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'ph',
+		'desc' => 'prop_sph.ph_desc',
+		'type' => 'textfield',
+		'options' => '',
+		'value' => '',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'sortby',
+		'desc' => 'prop_sph.ph_sortby',
+		'type' => 'textfield',
+		'options' => '',
+		'value' => 'menuindex',
+		'lexicon' => 'setplaceholders:default'
+	),
+	array(
+		'name' => 'sortdir',
+		'desc' => 'prop_sph.ph_sortdir',
+		'type' => 'list',
+		'options' => array(
+			array(
+				'name' => 'ASC',
+				'value' => 'ASC',
+				'menu' => '',
+			),
+			array(
+				'name' => 'DESC',
+				'value' => 'DESC',
+				'menu' => '',
+			)
+		),
+		'value' => 'ASC',
+		'lexicon' => 'setplaceholders:default'
+	)
 );
 
 return $properties;
--- core/components/setplaceholders/elements/snippets/setplaceholders.snippet.php
+++ core/components/setplaceholders/elements/snippets/setplaceholders.snippet.php
@@ -16,10 +16,9 @@
  * setPlaceholders; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
  *
- *
  * @package setPlaceholders
  * @author Jason Grant
- * @version 1.1.0-pl
+ * @version 1.2.0-pl
  */
 
 /**
@@ -28,21 +27,20 @@
  *
  * Variables
  * ---------
- *
  * @var modX $modx
  * @var input $input
  * @var options $options
  *
- *
  * Properties
  * ----------
- *
  * @property id - (integer)
- * @property vars - (string)
+ * @property ph - (string)
  * @property prefix - (string)
  * @property output - (boolean)
  * @property delimiter - (string)
  * @property placeholders - (string)
+ * @property sortby - (string)
+ * @property sortdir - (string)
  * @property fields - (string)  Deprecated.
  *
  * See the default properties for a description of each.
@@ -61,150 +59,175 @@ $ph = $ph ? explode('||', $ph) : array();
 $prefix = isset($prefix) ? $prefix : 'sph.';
 $delimiter = isset($delimiter) ? $delimiter : ',';
 $placeholders = $placeholders ? explode('||', $placeholders) : array();
-
+global $sortby, $sortdir;
+$sortby = $sortby ? $sortby : 'menuindex';
+$sortdir = $sortdir ? $sortdir : 'ASC';
 /* Deprecated as of v1.1 */
-$fields = $fields ? explode(',', $fields) : array();
+$fields = isset($fields) ? explode(',', $fields) : array();
 
-if ( !function_exists('getVal') ) {
-	function getVal($fieldName, $id) {
+if ( !function_exists('sph_getVal') ) {
+function sph_getVal($fieldName, $id) {
+	if ($fieldName[0] === '"') {  // check for a quoted value and skip parsing if found
+		if ( substr($fieldName, -1) === '"' ) { return( substr($fieldName, 1, -1) ); }  // remove a trailing " if present
+		else { return( $field[0] = substr($fieldName, 1) ); }
+	}
+	$fieldPrefixes = explode('.', $fieldName);
+	if ( $fieldPrefixes[0] === 'get' ) { return( $_GET[substr($fieldName, 4)] ); }
+	elseif ( $fieldPrefixes[0] === 'post' ) { return( $_POST[substr($fieldName, 5)] ); }
+	else {
 		global $modx;
-		static $cache = array(); // cache for resources and parent IDs
-
-		if ($fieldName[0] === '"') {  // check for a quoted value and skip parsing if found
-			if ( substr($fieldName, -1) === '"' ) {  // remove a trailing " if present
-				return( substr($fieldName, 1, -1) );
-			}
-			else {
-				return( $field[0] = substr($fieldName, 1) );
-			}
+		static $sph_cache = array(); // cache for resources and parent IDs
+		$idx = 0;
+		$fieldNameOffset = 0;
+		if ( is_numeric($fieldPrefixes[0]) ) {  // check for a resource ID
+			$id = (int) $fieldPrefixes[0];
+			$fieldNameOffset += strlen($fieldPrefixes[0]) + 1;
+			$idx = 1;
 		}
-
-		$fieldPrefixes = explode('.', $fieldName);
-
-		if ( $fieldPrefixes[0] === 'get' ) {
-			return( $_GET[substr($fieldName, 4)] );
+		if ( substr($fieldPrefixes[$idx], 0, 7) === 'Uparent' ) {  // Ultimate parent
+			$level = 2;
+			$r_index = substr($fieldPrefixes[$idx], 7);
+			if ( $r_index ) {  // read any Uparent index
+				$level = 1 + (int) $r_index;
+				$fieldNameOffset += strlen($r_index);
+			}
+			$cacheKey = $id . 'p';
+			if ( !isset($sph_cache[$cacheKey]) ) {
+				$sph_cache[$cacheKey] = $modx->getParentIds($id);
+				$sph_cache[$cacheKey . 'c'] = count($sph_cache[$cacheKey]);
+			}
+			$tmp = $sph_cache[$cacheKey . 'c'] - $level;
+			$id = $sph_cache[$cacheKey][ $tmp > 0 ? $tmp : 0 ];  // don't go past the immediate parent
+			$fieldNameOffset += 8;
+			++$idx;
 		}
-		elseif ( $fieldPrefixes[0] === 'post' ) {
-			return( $_POST[substr($fieldName, 5)] );
+		if ( substr($fieldPrefixes[$idx], 0, 6) === 'parent' ) {  // regular parent
+			$level = 0;
+			$r_index = substr($fieldPrefixes[$idx], 6);
+			if ( $r_index ) {  // ready any parent index
+				$level = -1 + (int) $r_index;
+				$fieldNameOffset += strlen($r_index);
+			}
+			$cacheKey = $id . 'p';
+			if ( !isset($sph_cache[$cacheKey]) ) {
+				$sph_cache[$cacheKey] = $modx->getParentIds($id);
+				$sph_cache[$cacheKey . 'c'] = count($sph_cache[$cacheKey]);
+			}
+			$tmp = $sph_cache[$cacheKey . 'c'] - 2;
+			$id = $sph_cache[$cacheKey][ $level < $tmp ? $level : $tmp ];  // don't go past top-most parent
+			$fieldNameOffset += 7;
+			++$idx;
 		}
-		else {
-			$idx = 0;
-			$fieldNameOffset = 0;
-			if ( is_numeric($fieldPrefixes[0]) ) {  // check for a resource ID
-				$id = (int) $fieldPrefixes[0];
-				$fieldNameOffset += strlen($fieldPrefixes[0]) + 1;
-				$idx = 1;
+		if ( substr($fieldPrefixes[$idx], 0, 5) === 'child' ) {  // child resource
+			$level = 0;
+			$r_index = substr($fieldPrefixes[$idx], 5);
+			if ( $r_index != 0 ) {  // ready any child index
+				$level = -1 + (int) $r_index;
+				$fieldNameOffset += strlen($r_index);
 			}
-			if ( substr($fieldPrefixes[$idx], 0, 7) === 'Uparent' ) {  // Ultimate parent
-				$uparent_level = 2;
-				$uparent = substr($fieldPrefixes[$idx], 7);
-				if ( $uparent ) {  // read any Uparent index
-					$uparent_level = 1 + (int) $uparent;
-					$fieldNameOffset += strlen($uparent);
+			$cacheKey = $id . 'c';
+			if ( !isset($sph_cache[$cacheKey]) ) {
+				$cids = array();
+				$c = $modx->newQuery( 'modResource', array('published' => 1, 'deleted' => 0, 'parent' => $id) );
+				$c->sortby($GLOBALS['sortby'], $GLOBALS['sortdir']);
+				$children = $modx->getCollection('modResource', $c);
+				if (empty($children)) { return 0; }
+				else {
+					foreach ($children as $child) { $cids[] = $child->get('id'); }  // store each child id in our array
+					unset($children);
 				}
-				$cacheKey = $id . 'pids';
-				if ( !isset($cache[$cacheKey]) ) {
-					$cache[$cacheKey] = $modx->getParentIds($id);
-				}
-				$tmp = count($cache[$cacheKey]) - $uparent_level;
-				$id = $cache[$cacheKey][ $tmp > 0 ? $tmp : 0 ];  // don't go past the immediate parent
-				$fieldNameOffset += 8;
-				++$idx;
+				$sph_cache[$cacheKey] = $cids;  // cache the array
+				$sph_cache[$cacheKey . 'c'] = count($cids) - 1;  // and its count
 			}
-			if ( substr($fieldPrefixes[$idx], 0, 6) === 'parent' ) {  // regular parent
-				$parent_level = 0;
-				$parent = substr($fieldPrefixes[$idx], 6);
-				if ( $parent ) {  // ready any parent index
-					$parent_level = -1 + (int) $parent;
-					$fieldNameOffset += strlen($parent);
-				}
-				$cacheKey = $id . 'pids';
-				if ( !isset($cache[$cacheKey]) ) {
-					$cache[$cacheKey] = $modx->getParentIds($id);
-				}
-				$tmp = count($cache[$cacheKey]) - 2;
-				$id = $cache[$cacheKey][ $parent_level < $tmp ? $parent_level : $tmp ];  // don't go past top-most parent
-				$fieldNameOffset += 7;
-				++$idx;
+			$cidsCount = $sph_cache[$cacheKey . 'c'];
+			if ($level > $cidsCount) { $level = $cidsCount; }  // don't go past the last child
+			elseif ($level < 0) {  // or the first
+				$cidsCount += 2;
+				$level = (-$level > $cidsCount) ? 0 : $level + $cidsCount;
 			}
-			$cacheKey = $id . 'id';
-			if ( isset($cache[$cacheKey]) ) {
-				$doc = $cache[$cacheKey];
+			$id = $sph_cache[$cacheKey][ $level ];
+			$fieldNameOffset += 6;
+			++$idx;
+		}
+		$cacheKey = $id . 'id';
+		if ( !isset($sph_cache[$cacheKey]) ) { $sph_cache[$cacheKey] = $modx->getObject('modResource', $id); }
+		if ( $doc = $sph_cache[$cacheKey] ) {  // if we've got a valid resource
+			if ( $fieldPrefixes[$idx] === 'tv' ) { return $doc->getTVValue( substr($fieldName, $fieldNameOffset + 3) ); }
+			elseif ( substr($fieldPrefixes[$idx], 0, 4) === 'migx' ) {  // get a migx TV (array of JSON objects)
+				$migx_rows = substr($fieldPrefixes[$idx], 4);  // check for an object limit
+				$migx_rows &&   $fieldNameOffset += strlen($migx_rows);
+				$migx = json_decode($doc->getTVValue( substr($fieldName, $fieldNameOffset + 5) ), TRUE);
+				$migx_rows &&   $migx = array_slice($migx, 0, (int) $migx_rows);
+				return $migx;
 			}
-			else {
-				$doc = $cache[$cacheKey] = $modx->getObject('modResource', $id);
-			}
-			if ($doc) {  // if we've got a valid resource
-				if ( $fieldPrefixes[$idx] === 'tv' ) {  // get a TV
-					return( $doc->getTVValue( substr($fieldName, $fieldNameOffset + 3) ) );
-				}
-				else {  // assume it's a field name
-					return( $doc->get( substr($fieldName, $fieldNameOffset) ) );
-				}
-			}
+			else { return $doc->get( substr($fieldName, $fieldNameOffset) ); }  // assume it's a field name
 		}
 	}
 }
-
+}
 
 $p = array();  // placeholder storage
-
 foreach ($ph as $field) {
 	$field = explode('!!', $field);  // separate out any default value
 	$varname = explode('==', $field[0]);  // separate out any user-defined placeholder name
-	if ( count($varname) === 1 ) {  // if there isn't one..
-		$varname = '';
-	}
+	if ( count($varname) === 1 ) { $varname = ''; }  // if there isn't one..
 	else {  // store the placeholder name
 		$field[0] = $varname[1];
 		$varname = trim( $varname[0] );
 	}
 	$fieldName = $field[0] = trim( $field[0] );
-
-	$value = getVal($fieldName, $id);
-
-	if ( $value == '' && isset($field[1]) ) {  // if we didn't find a value, use the default
-		$value = getVal( trim($field[1]), $id );
-	}
-	if ( $value != '') {
-		$p[ $varname ? $varname : $prefix . $field[0] ] = $value;  // key: user-defined nam OR prefix + field name
+	$value = sph_getVal($fieldName, $id);
+	if ( $value == '' && isset($field[1]) ) { $value = sph_getVal( trim($field[1]), $id ); }  // if we didn't find a value, use the default
+	if ($value != '') {
+		$varname = $varname ? $varname : $prefix . $field[0];
+		if (is_array($value)) {  // special processing for migx
+			$varname .= '.';
+			$migx_idx = 1;
+			foreach ($value as $migx_row) {
+				if (is_array($migx_row)) {
+					$migx_notfirst = FALSE;
+					foreach ($migx_row as $k=>$v) {  // set key:value pairs but ignore MIGX_id
+						if ($migx_notfirst || $k !== 'MIGX_id') { $p[$varname . $k . $migx_idx] = $v;}
+						$migx_notfirst = TRUE;
+					}
+					++$migx_idx;
+				}
+			}
+			$p[$varname . 'total'] = $migx_idx - 1;  // set a placeholder with the total # of objects processed
+		}
+		else { $p[$varname] = $value; }  // key: user-defined nam OR prefix + field name
 	}
 }
 
-/*
- * Code for deprecated property &fields.
- * Retained for backwards compatibility.
- */
-if ($fields)  {	$resource = $modx->getObject('modResource', $id); }
-$parents = array();
-foreach ($fields as $field) {
-	$field = explode('!!', $field);
-	$fieldName = $field[0] = trim( $field[0] );
-	$fieldPrefixes = explode('.', $fieldName);
-
-	$doc = $resource;
-	$value = NULL;
-
-	if ($fieldPrefixes[0] === 'get')  { $value = $_GET[substr($fieldName, 4)];	}
-	elseif ($fieldPrefixes[0] === 'post')  { $value = $_POST[substr($fieldName, 5)]; }
-	else {
-		for ($idx = 0; $fieldPrefixes[$idx] === 'parent' && $doc; ++$idx) {
-			if (!isset($parents[$idx]))  { $parents[] = $modx->getObject('modResource', $doc->get('parent')); }
-			$doc = $parents[$idx];
-			$fieldName = substr($fieldName, 7);
+/* Code for deprecated property &fields. Retained for backwards compatibility. */
+if ($fields)  {
+	$resource = $modx->getObject('modResource', $id);
+	$parents = array();
+	foreach ($fields as $field) {
+		$field = explode('!!', $field);
+		$fieldName = $field[0] = trim( $field[0] );
+		$fieldPrefixes = explode('.', $fieldName);
+		$doc = $resource;
+		$value = NULL;
+		if ($fieldPrefixes[0] === 'get')  { $value = $_GET[substr($fieldName, 4)];	}
+		elseif ($fieldPrefixes[0] === 'post')  { $value = $_POST[substr($fieldName, 5)]; }
+		else {
+			for ($idx = 0; $fieldPrefixes[$idx] === 'parent' && $doc; ++$idx) {
+				if (!isset($parents[$idx]))  { $parents[] = $modx->getObject('modResource', $doc->get('parent')); }
+				$doc = $parents[$idx];
+				$fieldName = substr($fieldName, 7);
+			}
+			if ($doc) {
+				if ($fieldPrefixes[$idx] === 'tv')  { $value = $doc->getTVValue( substr($fieldName, 3) ); }
+				else { $value = $doc->get($fieldName); }
+			}
 		}
-		if ($doc) {
-			if ($fieldPrefixes[$idx] === 'tv')  { $value = $doc->getTVValue( substr($fieldName, 3) ); }
-			else { $value = $doc->get($fieldName); }
-		}
+		if ($value == '' && isset($field[1]))  { $value = trim( $field[1] ); }
+		if ($value != '')  { $p[ $prefix . $field[0] ] = $value; }
 	}
-
-	if ($value == '' && isset($field[1]))  { $value = trim( $field[1] ); }
-	if ($value != '')  { $p[ $prefix . $field[0] ] = $value; }
 }
-/*
- * End legacy code
- */
+/* End legacy code */
+
 foreach ($placeholders as $placeholder) { // add any user-defined placeholders
 	$ph = explode('==', $placeholder);
 	$p[ trim($ph[0]) ] = trim($ph[1]);
@@ -212,4 +235,6 @@ foreach ($placeholders as $placeholder) { // add any user-defined placeholders
 
 // Output our results
 $modx->setPlaceholders($p);
-return ($output ? implode($delimiter, $p) : '');
+$output = $output ? implode($delimiter, $p) : '';
+
+return $output;
